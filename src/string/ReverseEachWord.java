b45c8861de9dfddbbf57013b0d9cd5dd65718e50
IRFAN M S <i.khaisarbashams@dxc.com>
2024-10-30 19:43:51 +0530

    test

--- src/string/ReverseEachWord.java
+++ src/string/ReverseEachWord.java
@@ -1,19 +1,4 @@
 package string;
 
-import java.util.Scanner;
-
 public class ReverseEachWord {
-
-	public static void main(String[] args) {
-		Scanner s1 = new Scanner(System.in);
-		String str1 = s1.nextLine();
-		String[] words = str1.split(" ");
-		char c;
-		for (int i = 0; i < words.length; i++) {
-			String reveachword = "";
-			for(int j=0; j<)
-
-		}
-
-	}
 }
